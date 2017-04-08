18e8f5dc8e42d2cbe16131f472a503fd1d44cd72
Yildi Johana Ruiz Zapata <yildi.ruiz@ceiba.com.co>
2017-04-08 10:39:05 -0500

    Primer commit

---
+++ proyecto1/src/main/java/co/ceiba/domain/Person.java
@@ -0,0 +1,18 @@
+package co.ceiba.domain;
+
+public class Person {
+
+	private String name;
+	private String lastName;
+	
+	public Person(String name, String lastName){
+		this.name = name;
+		this.lastName = lastName;
+	}
+	public String getName(){
+		return name;
+	}
+	public String gestLastName(){
+		return lastName;
+	}
+}
---
+++ proyecto1/src/main/java/co/ceiba/service/NotifyPersonService.java
@@ -0,0 +1,17 @@
+package co.ceiba.service;
+
+import co.ceiba.domain.Person;
+
+public class NotifyPersonService {
+	
+	private EmailService emailService;
+	
+	public NotifyPersonService(EmailService emailService){
+		this.emailService = emailService;
+	}
+	
+	public String notify(Person person){
+		String message = "welcome" + person.getName();
+		return emailService.sendMail(message);
+	}
+}
